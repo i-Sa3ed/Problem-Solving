0ceece6311ff18e82d238ddeab2585dd4a909184
i-khadra <saeed.developer18@gmail.com>
2023-02-12 08:32:21 +0200

    added: Modular Arithmetics Template

--- Functions/Mathematical Functions.cpp
+++ Functions/Mathematical Functions.cpp
@@ -141,3 +141,88 @@ double evaluate_postfix_exp(string exp) {
 
     return stk.top();
 }
+
+///////////////////// Modular Arithmetics Template //////////////////////////////////////
+
+template<int mod = MOD>
+struct mint {
+private:
+    // assume -P <= x < 2P, (it's faster)
+    int norm(int x) const {
+        if (x < 0)
+            x += mod;
+        if (x >= mod)
+            x -= mod;
+        return x;
+    }
+
+public:
+    int x;
+    mint(int32_t x = 0) : x(norm(x)) {}
+    mint(ll x) : x(norm(x % mod)) {}
+    int val() const { return x; }
+    mint operator-() const { return mint(norm(mod - x)); }
+    mint inv() const {
+        assert(x != 0);
+        return power(mod - 2);
+    }
+    mint power(ll b) const {
+        mint res = 1, a = x;
+        while (b) {
+            if (b & 1) res *= a;
+            a *= a;
+            b >>= 1;
+        }
+        return res;
+    }
+    mint &operator*=(const mint &rhs) {
+        x = (ll)x * rhs.x % mod;
+        return *this;
+    }
+    mint &operator+=(const mint &rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    mint &operator-=(const mint &rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    mint &operator/=(const mint &rhs) { return *this *= rhs.inv(); }
+    friend mint operator*(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend mint operator+(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend mint operator-(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend mint operator/(const mint &lhs, const mint &rhs) {
+        mint res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend std::istream &operator>>(std::istream &is, mint &a) {
+        ll v;
+        is >> v;
+        a = mint(v);
+        return is;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const mint &a) {
+        return os << a.val();
+    }
+    friend mint max(mint a, mint b) {
+        return a.x > b.x ? a : b;
+    }
+    friend mint min(mint a, mint b) {
+        return a.x < b.x ? a : b;
+    }
+};
+
+using Z = mint<>;

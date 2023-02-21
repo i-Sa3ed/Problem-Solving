00b1a671a5bedd9fda97eb0962d0d279e3611aa3
i-khadra <saeed.developer18@gmail.com>
2023-02-21 11:37:59 +0200

    updated: rectangle

--- Geometry/Point.cpp
+++ Geometry/Point.cpp
@@ -1,17 +1,19 @@
-struct point {
+struct Point {
     double x, y;
-    point(double x = 0, double y = 0) : x(x), y(y) {
+    Point(double x = 0, double y = 0) : x(x), y(y) {
     }
 
-    point operator -(point p) {
-        point res;
+    void read() { cin >> x >> y; }
+
+    Point operator -(Point p) {
+        Point res;
         res.x = x - p.x;
         res.y = y - p.y;
 
         return res;
     }
 };
-double length(point p) {
+double length(Point p) {
     return sqrt(p.x * p.x + p.y * p.y);
     // notice that it can be generalized to any #dimensions
 }
--- Geometry/Rectangle.cpp
+++ Geometry/Rectangle.cpp
@@ -1,7 +1,54 @@
-struct Rectangle {
+struct Point {
+    double x, y;
+    Point(double x = 0, double y = 0) : x(x), y(y) {
+    }
+
+    void read() { cin >> x >> y; }
+
+    Point operator -(Point p) {
+        Point res;
+        res.x = x - p.x;
+        res.y = y - p.y;
+
+        return res;
+    }
+};
+
+struct Rectangle_ul_lr {
+    Point ul, lr; // upper left, lower right
+    double w, h; // width, length
+    Rectangle_ul_lr() { w = h = 0; }
+
+    void read()
+    { cin >> ul.x >> ul.y >> lr.x >> lr.y; }
+    void compute() {
+        w = lr.x - ul.x;
+        h = ul.y - lr.y;
+    }
+    double area() {
+        compute();
+        return (w * h);
+    }
+
+    bool is_inside(Point p, bool consider_border = false) {
+        bool res;
+        if (!consider_border) {
+            res = (p.x > ul.x and p.x < lr.x)
+                  and (p.y > lr.y and p.y < ul.y);
+        }
+        else {
+            res = (p.x >= ul.x and p.x <= lr.x)
+                  and (p.y >= lr.y and p.y <= ul.y);
+        }
+
+        return res;
+    }
+};
+
+struct Rectangle_bl_tr {
     Point bl, tr;
     ll w, h; // width, length
-    Rectangle() { w = h = 0; }
+    Rectangle_bl_tr() { w = h = 0; }
 
     void read()
     { cin >> bl.x >> bl.y >> tr.x >> tr.y; }
@@ -15,7 +62,7 @@ struct Rectangle {
     };
 };
 
-ll inter_area(Rectangle r1, Rectangle r2)
+ll inter_area(Rectangle_bl_tr r1, Rectangle_bl_tr r2)
 {
     ll iw, il; // intersection width and length
     iw = max(0, min(r1.tr.x, r2.tr.x) - max(r1.bl.x, r2.bl.x));
@@ -24,11 +71,11 @@ ll inter_area(Rectangle r1, Rectangle r2)
     return (iw * il);
 }
 
-ll inter_area(vector<Rectangle>& rectangles)
+ll inter_area(vector<Rectangle_bl_tr>& rectangles)
 {
     Point max_bl, min_tr;
     min_tr.x = min_tr.y = INT_MAX;
-    for(Rectangle& rect : rectangles) {
+    for(Rectangle_bl_tr& rect : rectangles) {
         max_bl.x = max(max_bl.x, rect.bl.x);
         max_bl.y = max(max_bl.y, rect.bl.y);
         min_tr.x = min(min_tr.x, rect.tr.x);

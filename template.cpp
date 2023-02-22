81437ee942780285334197cf64e10a74a8c63fed
i-khadra <saeed.developer18@gmail.com>
2023-02-22 10:48:08 +0200

    added: template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// debug:
+#ifndef ONLINE_JUDGE
+#include "D:/Problem Solving/Some related files/debug/debug.hpp"
+#else
+#define debug(...)
+#define debug_itr(...)
+#define debug_bits(...)
+#endif
+
+int MOD = 1e9 + 7;
+using ll = long long;
+#define vi2d(x)  vector< vector<int> > x;
+#define v2d_resize(x, n, m) x = vector< vector<int> >(n, vector<int>(m));
+string letters = "abcdefghijklmnopqrstuvwxyz";
+#define Saeed_fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define endl '\n'  // because '\n' is faster while compiling, and 'endl' is easier for coding ;)
+#define all(c) c.begin(), c.end()
+#define all1(a, n) (a.begin() + 1), (a.begin() + n + 1)
+void y_n(bool ok) { cout << (ok? "YES\n" : "NO\n"); }
+
+/// Global Variables ///
+
+/// Don't forget the REINITIALIZATION man !! ///
+
+void solve(int TC) {
+
+}
+
+int main()
+{
+    Saeed_fast;
+
+    int TC = 1; cin >> TC;
+    while(TC--) { solve(TC); }
+
+    return 0;
+}

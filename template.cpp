c402baf515df391a59766a0487c83df42adb8484
i-khadra <saeed.developer18@gmail.com>
2023-03-19 16:39:03 +0200

    modify template

--- template.cpp
+++ template.cpp
@@ -10,18 +10,14 @@ using namespace std;
 #define debug_bits(...)
 #endif
 
-int MOD = 1e9 + 7;
 using ll = long long;
-string letters = "abcdefghijklmnopqrstuvwxyz";
 #define Saeed_fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
 #define endl '\n'  // because '\n' is faster while compiling, and 'endl' is easier for coding ;)
 #define all(c) c.begin(), c.end()
 #define all1(a, n) (a.begin() + 1), (a.begin() + n + 1)
 void y_n(bool ok) { cout << (ok? "YES\n" : "NO\n"); }
 
-/// Global Variables ///
-
-/// Don't forget the REINITIALIZATION man !! ///
+/////
 
 void solve(int TC) {
 
@@ -32,7 +28,9 @@ int main()
     Saeed_fast;
 
     int TC = 1; cin >> TC;
-    while(TC--) { solve(TC); }
+    for (int test = 1; test <= TC; ++test) {
+        solve(test);
+    }
 
     return 0;
 }

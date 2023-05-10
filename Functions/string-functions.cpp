e4c89a145ca6608da9f9c395b72f5248b7356321
Saeed Khadra <saeed.developer18@gmail.com>
2023-05-10 16:01:44 +0300

    modify

--- Functions/string-functions.cpp
+++ Functions/string-functions.cpp
@@ -1,5 +1,14 @@
 ////////////////////////////// Palindrome Stuff /////////////////////////////
 
+bool is_palindrome(string& s) {
+    int n = s.size();
+    for (int i = 0; i < n/2; ++i) {
+        if (s[i] != s[n-1-i])
+            return false;
+    }
+    return true;
+}
+
 string build_palindrome_min_lexi(vector<int>& frq) {
     string half;
     char odd = '*';
--- template.cpp
+++ template.cpp
@@ -13,9 +13,12 @@ using namespace std;
 using ll = long long;
 #define Saeed_fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
 #define endl '\n'  // because '\n' is faster while compiling, and 'endl' is easier for coding ;)
-#define all(c) c.begin(), c.end()
-#define all1(a, n) (a.begin() + 1), (a.begin() + n + 1)
-void y_n(bool ok) { cout << (ok? "YES\n" : "NO\n"); }
+#define all(c) (c).begin(), (c).end()
+#define all1(a, n) ((a).begin() + 1), ((a).begin() + (n) + 1)
+#define for1(iter, n) for (int (iter) = 1; (iter) <= (n); ++(iter))
+
+void yes_or_no(bool ok) { cout << (ok? "YES\n" : "NO\n"); }
+ll sum_to_n(ll n) { return n*(n + 1)/2; }
 
 /////
 

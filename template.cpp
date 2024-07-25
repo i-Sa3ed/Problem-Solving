7c0a10547dd80e41fba1e007781960e2493650ce
Saeed Ahmed <92569917+i-Sa3ed@users.noreply.github.com>
2024-07-25 23:46:46 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -21,14 +21,11 @@ typedef vector< vector<int> > GRAPH;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
             tree_order_statistics_node_update> indexed_set; // find_by_order, order_of_key
 
-#define Saeed_fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define fast ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
 #define endl '\n'  // because '\n' is faster while compiling, and 'endl' is easier for coding ;)
 #define all(c) (c).begin(), (c).end()
-#define all1(a, n) ((a).begin() + 1), ((a).begin() + (n) + 1)
 #define rep(i, n) for(int (i) = 0; (i) < n; ++(i))
-#define rep1(i, n) for (int (i) = 1; (i) <= n; ++(i))
 
-inline void yn(bool ok) { cout << (ok? "YES\n" : "NO\n"); }
 inline ll sum_to_n(ll n) { return n*(n + 1)/2; }
 // double compare
 // returns: 0 if almost equal, 1 if `a` greater, -1 if `b` greater
